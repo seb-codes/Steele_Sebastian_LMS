48edbc5733559783e80a930e157e896a28193960
Seb <59701678+seb-codes@users.noreply.github.com>
2023-11-03 16:09:20 -0400

    Add files via upload

--- Module2/src/LibraryManagementSystem.java
+++ Module2/src/LibraryManagementSystem.java
@@ -1,7 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.time.LocalDate;
 
 public class LibraryManagementSystem {
@@ -11,8 +9,16 @@ public class LibraryManagementSystem {
        This program manages the books in a library through multiple abilities.
        add books from a list in a text file, remove books by the id, and list all current books in the library
      */
-    static final List<Book> bookList = new ArrayList<>();
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        LibraryManagementSystem myInstance = new LibraryManagementSystem();
+        myInstance.lms();
+
+    }
+
+    public List<Book> bookList = new ArrayList<>();
+
+    public void lms() throws IOException {
         while (true) {
             System.out.println("Welcome to the Library Management System.\n Select from the options below:");
             System.out.println("1. Add a book");
@@ -31,26 +37,32 @@ public class LibraryManagementSystem {
             scanner.nextLine();
 
             switch (choice) {
-                case 1 -> addBook(scanner);
-                case 2 -> removeBook(scanner);
+                case 1 -> addBook();
+                case 2 -> removeBook();
                 case 3 -> listBooks();
-                case 4 -> addlibraryFile(scanner);
-                case 5-> printLibrary();
-                case 6 -> checkoutBook(scanner);
-                case 7 -> checkinBook(scanner);
+                case 4 -> addlibraryFile();
+                case 5 -> printLibrary();
+                case 6 -> checkoutBook();
+                case 7 -> checkinBook();
                 case 0 -> System.exit(0);
                 default -> System.out.println("Invalid choice. Please try again.");
             }
+
+
         }
     }
 
-    static void addBook(Scanner scanner) {
+
+    public void addBook() throws IOException {
         /*
         addBook, this method allows you to add a book into your library
         args: Scanner scanner, allows you to scan in values from user
         ret: Void
          */
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter book data (format: id,title,author) or type 'exit' to quit:");
+        LibraryManagementSystem.Book newBook = null;
         while (true) {
             System.out.print("> ");
             String inputLine = scanner.nextLine().trim();
@@ -60,14 +72,33 @@ public class LibraryManagementSystem {
             }
 
             String[] parts = inputLine.split(",");
-            if (parts.length == 4) {
-                try{
-                    String title = parts[0];
-                    String author = parts[1];
-                    int barcode = Integer.parseInt(parts[2]);
-                    String genre = parts[3];
+            if (parts.length == 3) {
+                try {
+                    int id = Integer.parseInt(parts[0]);
+                    String title = parts[1];
+                    String author = parts[2];
                     //System.out.println(parts[0] + parts[1] + parts[2]);
-                    bookList.add(new Book(title, author, barcode, genre));
+
+                    boolean dupBook = false;
+
+                    for (Book book : bookList) {
+                        // Access each book instance using the 'book' variable.
+                        if (title.equals(book.getTitle())) {
+                            dupBook = true;
+                            break;
+                        }
+                        // Add more code to process each book as needed.
+                    }
+
+                    if (dupBook) {
+                        System.out.println("Books already exist in Library can not add again");
+                        break;
+
+                    }
+
+                    newBook = new Book(id, title, author);
+                    bookList.add(newBook);
+                    break;
                 } catch (NumberFormatException e) {
                     System.out.println("Invalid input format. Please use format: id,title,author");
                 }
@@ -78,50 +109,153 @@ public class LibraryManagementSystem {
 
     }
 
-    static void printLibrary(){
+    public void printLibrary() {
 
         if (bookList.isEmpty()) {
             System.out.println("The Library is empty.");
         } else {
             System.out.println("\n\nPrinting out Books in the Library");
-                for (Book book : bookList) {
-                    System.out.println(book.toTxt()); // Add a newline separator between entries
+            for (Book book : bookList) {
+                System.out.println(book.toTxt()); // Add a newline separator between entries
             }
             System.out.println("\n\n");
         }
 
     }
 
-    private static void addlibraryFile(Scanner scanner){
-            System.out.println("Each row in file must be in the following format \n title,author,barcode,genre \n status defaults to checked in and due date defaults to null");
-            System.out.print("Enter the file path: ");
-            String filePath = scanner.nextLine();
+    public void addlibrary(String filePath) {
 
-            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    String[] parts = line.split(",");
-                    if (parts.length == 4) {
-                        String title = parts[0];
-                        String author = parts[1];
-                        int barcode = Integer.parseInt(parts[2]);
-                        String genre = parts[3];
-                        //System.out.println(parts[0] + parts[1] + parts[2]);
-                        bookList.add(new Book(title, author, barcode, genre));
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                boolean dupBook = false;
+                String[] parts = line.split(",");
+                if (parts.length == 3) {
+                    int id = Integer.parseInt(parts[0]);
+                    String title = parts[1];
+                    String author = parts[2];
+
+                    for (Book book : bookList) {
+                        // Access each book instance using the 'book' variable.
+
+                        if (title.equals(book.getTitle())) {
+                            dupBook = true;
+                            System.out.println("Title: " + book.getTitle() + " Found Duplicate");
+                            break;
+                        }
+                        // Add more code to process each book as needed.
                     }
+
+                    if (dupBook) {
+                        System.out.println("Books already exist in Library can not add again");
+                        continue;
+                    }
+
+
+                    //System.out.println(parts[0] + parts[1] + parts[2]);
+                    bookList.add(new Book(id, title, author));
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-    };
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addlibraryFile() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Each row in file must be in the following format \n title,author,barcode,genre \n status defaults to checked in and due date defaults to null");
+        System.out.print("Enter the file path: ");
+        String filePath = scanner.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                boolean dupBook = false;
+                String[] parts = line.split(",");
+                if (parts.length == 3) {
+                    int id = Integer.parseInt(parts[0]);
+                    String title = parts[1];
+                    String author = parts[2];
+
+                    for (Book book : bookList) {
+                        // Access each book instance using the 'book' variable.
+
+                        if (title.equals(book.getTitle())) {
+                            dupBook = true;
+                            System.out.println("Title: " + book.getTitle() + " Found Duplicate");
+                            break;
+                        }
+                        // Add more code to process each book as needed.
+                    }
+
+                    if (dupBook) {
+                        System.out.println("Books already exist in Library can not add again");
+                        continue;
+                    }
 
 
-    static void removeBook(Scanner scanner){
+                    //System.out.println(parts[0] + parts[1] + parts[2]);
+                    bookList.add(new Book(id, title, author));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // Define a method to get library book titles as an array of strings
+    public String[] getLibraryBookTitles() {
+        String[] titles = new String[bookList.size()];
+        if (bookList.isEmpty()) {
+            System.out.println("The Library is empty.");
+            return titles;
+        }
+
+
+        for (int i = 0; i < bookList.size(); i++) {
+            titles[i] = bookList.get(i).title;
+        }
+        return titles;
+    }
+
+    public void removeBookByBarcode(int barcode){
+        bookList.removeIf(book -> book.getBarcode()==barcode);
+    }
+
+    public void removeBookByTitle(String titleToRemove){
+        bookList.removeIf(book -> book.getTitle().equals(titleToRemove));
+
+    }
+
+    public List<Integer> getLibraryBookBarcodes(){
+
+        List<Integer> barcodes = new ArrayList<>();
+
+        if (bookList.isEmpty()) {
+            System.out.println("The Library is empty.");
+            return barcodes;
+        }
+
+        for (Book book : bookList) {
+            barcodes.add(book.getBarcode());
+        }
+
+        return barcodes;
+    }
+
+
+
+    public  void removeBook(){
        /*
         removeBook, this method allows you to remove a book from your library
         args: Scanner scanner, allows you to scan in values from user
         ret: Void
          */
+
+        Scanner scanner = new Scanner(System.in);
 
         List<Book> bookdupeList = new ArrayList<>();
 
@@ -207,7 +341,7 @@ public class LibraryManagementSystem {
 
     }
 
-    private static void listBooks(){
+    public void listBooks(){
         /*
         listBooks, this method creates a text file and writes each book in csv format to it
         args: None
@@ -229,7 +363,10 @@ public class LibraryManagementSystem {
         }
     }
 
-    private static void checkoutBook(Scanner scanner){
+    public void checkoutBook(){
+        Scanner scanner = new Scanner(System.in);
+
+
         System.out.print("Enter the title of the book to checkout: ");
         String titleToCheckout = scanner.nextLine().toLowerCase();
 
@@ -266,7 +403,28 @@ public class LibraryManagementSystem {
 
     }
 
-    private static void checkinBook(Scanner scanner){
+    public void checkoutBook(String titleToCheckout){
+
+        if(bookList.isEmpty()){
+            System.out.println("Book Title is not in the library or Already checked out");
+            return;
+        }
+
+
+        for (Book book : bookList) {
+            if (book.getTitle().equals(titleToCheckout) && book.getStatus().equals("checked in")) {
+                book.status = "checked out";
+                book.duedate = LocalDate.now().plusWeeks(4);
+                System.out.println("Book checked out.");
+                return;
+            }
+        }
+
+    }
+
+    public  void checkinBook(){
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter the title of the book to checkin: ");
         String titleToCheckin = scanner.nextLine().toLowerCase();
 
@@ -303,8 +461,72 @@ public class LibraryManagementSystem {
 
     }
 
+    public  void checkinBook(String titleToCheckin){
 
-     static class Book {
+        if(bookList.isEmpty()){
+            System.out.println("Book Title is not in the library or Already checked in");
+        }
+
+
+
+        for (Book book : bookList) {
+            if (book.getTitle().equals(titleToCheckin) && book.getStatus().equals("checked out")) {
+                book.status = "checked in";
+                book.duedate = null;
+            }
+        }
+
+    }
+
+    public String getLibraryBooksAsString(){
+        String ret_string = "";
+        if (bookList.isEmpty()) {
+            return("The Library is empty.");
+        } else {
+
+            for (Book book : bookList) {
+                ret_string+=(book.toTxt()+'\n'); // Add a newline separator between entries
+
+            }
+            return ret_string;
+        }
+
+    }
+
+    public List<String> getAvailableBookTitles(){
+        List<String> titles = new ArrayList<String>();
+        if (bookList.isEmpty()) {
+            System.out.println("The Library is empty.");
+            return titles;
+        }
+
+
+        for (Book book: bookList) {
+            if(book.getStatus().equals("checked in")){
+                titles.add(book.title);
+            }
+        }
+        return titles;
+    }
+
+    public List<String> getNotAvailableBookTitles(){
+        List<String> titles = new ArrayList<String>();
+        if (bookList.isEmpty()) {
+            System.out.println("The Library is empty.");
+            return titles;
+        }
+
+
+        for (Book book: bookList) {
+            if(book.getStatus().equals("checked out")){
+                titles.add(book.title);
+            }
+        }
+        return titles;
+    }
+
+
+     public static class Book {
         /* Sebastian Steele CEN 3024 Sept. 3 2023
            Class: Book
            This class contains information regarding books in a library.
@@ -322,15 +544,32 @@ public class LibraryManagementSystem {
         private LocalDate duedate;
 
 
-        public Book(String title, String author, int barcode, String genre) {
-            this.id = idCounter++;
+        public Book(int id,String title, String author) {
+            this.id = id;
             this.title = title;
             this.author = author;
-            this.barcode = barcode;
-            this.genre = genre;
+            this.barcode = generateRandomBarcode();
+            this.genre = generateRandomGenre();
             this.status = "checked in";
             this.duedate = null;
         }
+         // Generate a random barcode between 4 and 6 digits
+         private int generateRandomBarcode() {
+             Random random = new Random();
+             int minDigits = 1000; // Smallest 4-digit number
+             int maxDigits = 999999; // Largest 6-digit number
+             return random.nextInt(maxDigits - minDigits + 1) + minDigits;
+         }
+
+         // Generate a random book genre from a predefined list
+         private String generateRandomGenre() {
+             String[] genres = {"Mystery", "Science Fiction", "Fantasy", "Romance", "Thriller", "Non-Fiction", "Historical Fiction", "Adventure"};
+             Random random = new Random();
+             int index = random.nextInt(genres.length);
+             return genres[index];
+         }
+
+
 
         public int getId() {
             return id;
